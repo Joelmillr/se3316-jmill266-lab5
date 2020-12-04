57e1853d06a26ba0bfcdae0f332a2a020aa2ddaa
Joel <Jmill266@uwo.ca>
2020-12-03 21:29:36 -0500

    updated header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { HeaderTitleComponent } from './layout/header/header-title/header-title.component';
-import { HeaderSignInComponent } from './layout/header/header-sign-in/header-sign-in.component';
 import { HeaderImageComponent } from './layout/header/header-image/header-image.component';
 import { BodyComponent } from './layout/body/body.component';
 import { AboutComponentComponent } from './layout/body/about-component/about-component.component';
@@ -16,7 +15,6 @@ import { SignInComponentComponent } from './layout/body/sign-in-component/sign-i
     AppComponent,
     HeaderComponent,
     HeaderTitleComponent,
-    HeaderSignInComponent,
     HeaderImageComponent,
     BodyComponent,
     AboutComponentComponent,

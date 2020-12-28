8c4dc677832c1883f06ae002cb067d7206aa6744
Joel <Jmill266@uwo.ca>
2020-12-28 18:26:10 -0500

    more backend functionality

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,7 @@ const Schedule = require('./models/schedule');
 
 const Account = require('./models/account');
 const { scheduled } = require('rxjs');
+const course = require('./models/course');
 
 const app = express();
 
@@ -20,7 +21,7 @@ mongoose.connect("mongodb+srv://user0:vuVYcNGUzTRhtag7@cluster0.urhpg.mongodb.ne
 
 const connection = mongoose.connection;
 
-connection.once("open", function() {
+connection.once("open", function () {
   console.log("Connected to database!");
 });
 
@@ -31,19 +32,18 @@ app.use((req, res, next) => {
   res.header(
     'Access-Control-Allow-Headers',
     "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header(
-      'Access-Control-Allow-Methods',
-      `GET, POST, PUT, PATCH, DELETE, OPTIONS`
-    );
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    `GET, POST, PUT, PATCH, DELETE, OPTIONS`
+  );
   console.log(`${req.method} request for ${req.url}`);
   next();
 })
 
 // List of all Courses. Returns the list of Courses offered
-app.get('/api/courses', (req,res) => {
+app.get('/api/courses', (req, res) => {
   Course.find().then(courseList => {
-    console.log(subjects)
     res.status(200).json({
       message: "Courses fetched successfully!",
       courses: courseList
@@ -54,7 +54,7 @@ app.get('/api/courses', (req,res) => {
 // Get all course codes (property name: catalog_nbr) for a given subject code. Return an error if the subject code doesn’t exist.
 app.get(`/api/courses/:subject`, (req, res) => {
   const subject = req.params.subject;
-  Course.find({subject: {$regex: `${subject}`, $options: 'i'}}).then(courseList => {
+  Course.find({ subject: { $regex: `${subject}`, $options: 'i' } }).then(courseList => {
     res.status(200).json({
       message: `Showing results for subject = ${subject}`,
       courses: courseList
@@ -68,7 +68,7 @@ app.get('/api/courses/:subject/:catalog_nbr', (req, res) => {
   // No course component in the parameters
   const subject = req.params.subject;
   const catalog_nbr = req.params.catalog_nbr;
-  Course.find({subject: {$regex: `${subject}`, $options: 'i'}, catalog_nbr: {$regex: `${catalog_nbr}`, $options: 'i'}}).then(courseList => {
+  Course.find({ subject: { $regex: `${subject}`, $options: 'i' }, catalog_nbr: { $regex: `${catalog_nbr}`, $options: 'i' } }).then(courseList => {
     res.status(200).json({
       message: `Showing results for subject = ${subject}, catalog_nbr = ${catalog_nbr}`,
       courses: courseList
@@ -77,16 +77,62 @@ app.get('/api/courses/:subject/:catalog_nbr', (req, res) => {
 });
 
 // Create a new schedule (to save a list of courses) with a given schedule name. Return an error if name exists.
-app.post('/api/schedule/createSchedule/:scheduleName', (req, res) => {
-    const scheduleName = req.params.scheduleName;
-    const schedule = new Schedule({
-      title: scheduleName,
-      creator: req.body.creator,
-    })
-    res.status(201).json({
-      message: "Schedule added successfully",
-      schedule: schedule
-    })
+app.post('/api/schedules/createSchedule/:scheduleName', (req, res) => {
+  Schedule.exists({ title: req.params.scheduleName, creator: req.body.creator }, (err, result) => {
+    if (result) {
+      res.status(400).json({
+        message: "Schedule already Exists!",
+      })
+    } else {
+      const schedule = new Schedule({
+        title: req.params.scheduleName,
+        creator: req.body.creator,
+        public: req.body.public,
+        courseList: [],
+      })
+      schedule.save();
+      res.status(201).json({
+        message: "Schedule added successfully ",
+        schedule: schedule
+      })
+    }
+  })
 });
+
+// Save a list of subject code, course code pairs under a given schedule name. Return an error if the schedule name does not exist. Replace existing
+// subject-code + course-code pairs with new values and create new pairs if it doesn’t exist. [10 points]
+app.put('/api/schedules/editSchedule/:scheduleId', (req, res, next) => {
+  const id = req.params.scheduleId
+  const addCourse = req.body.course[0]
+
+  const updateCouseList = {
+    $push: { "courseList": addCourse }
+  }
+
+  Schedule.findByIdAndUpdate(id, {
+    $addToSet: { courseList: addCourse }
+  }, function (err, result) {
+    if (err) {
+      console.log(err);
+      return console.log('error');
+    } else {
+      console.log(result)
+      console.log('Updated schedule');
+      res.status(200).json({
+        message: 'Schedule updated'
+      })
+    }
+  });
+})
+
+// returns a list of all public schedules
+app.get('/api/schedules', (req, res) => {
+  Schedule.find({ public: true }).then(scheduleList => {
+    res.status(200).json({
+      message: 'Showing all public schedules',
+      schedules: scheduleList
+    })
+  })
+})
 
 module.exports = app;
--- backend/models/schedule.js
+++ backend/models/schedule.js
@@ -1,19 +1,23 @@
 const mongoose = require('mongoose');
 const joi = require('joi');
 
+const course_infoSchema = mongoose.Schema({
+  "days": [String],
+    "start_time": String,
+    "end_time": String,
+}, {_id: false});
+
+const courseListSchema = mongoose.Schema({
+  "subject": joi.string().regex(/^[^*|\":<>[\]{}`\\()';@&$?!]+$/).min(1), //only a-z, A-Z, 0-9. No spaces allowed either.,,
+  "catalog_nbr": joi.string().regex(/^[^*|\":<>[\]{}`\\()';@&$?!]+$/).min(1), //only a-z, A-Z, 0-9. No spaces allowed either.,,
+  "course_info": [course_infoSchema],
+},{_id: false});
+
 const scheduleSchema = mongoose.Schema({
   "title": joi.string().regex(/^[^*|\":<>[\]{}`\\()';@&$?!]+$/).min(1).required(), //only a-z, A-Z, 0-9. No spaces allowed either.,,
   "creator": joi.string().regex(/^[^*|\":<>[\]{}`\\()';@&$?!]+$/).min(1).required(), //only a-z, A-Z, 0-9. No spaces allowed either.,,
-  "courseList":[{
-    "subject": joi.string().regex(/^[^*|\":<>[\]{}`\\()';@&$?!]+$/).min(1).required(), //only a-z, A-Z, 0-9. No spaces allowed either.,,
-    "catalog_nbr": joi.string().regex(/^[^*|\":<>[\]{}`\\()';@&$?!]+$/).min(1).required(), //only a-z, A-Z, 0-9. No spaces allowed either.,,
-    "course_info": [{
-      "days": [String],
-      "start_time": String,
-      "end_time": String,
-    }],
-  }],
+  "courseList":[courseListSchema],
   "public": {type: Boolean, default: false}
-})
+}, {collection: 'Schedules'});
 
 module.exports = mongoose.model('Schedule', scheduleSchema);
